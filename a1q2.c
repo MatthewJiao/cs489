dab1f093e104f006b1081e094c7c5c79346dfc2f
Matthew <jiaomatthew222@gmail.com>
2025-02-18 16:05:04 -0500

    add fix

--- a1q2.c
+++ a1q2.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-    
-#define BUFSIZE 256
+#include <sys/stat.h> 
+
+// #define BUFSIZE 256
     
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
@@ -10,7 +11,16 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+    // char cmd[BUFSIZE] = "wc -c < ";
+    // strcat(cmd, argv[1]);
+    // system(cmd);
+
+    struct stat info;
+    if (stat(argv[1], &info) != 0) {
+        fprintf(stderr, "can't find file %s.\n", argv[1]);
+        return -1;
+    }
+
+    printf("%ld\n", info.st_size);
+    return 0;
 }
